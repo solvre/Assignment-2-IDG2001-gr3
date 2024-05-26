7dace0804c987728db92f975248f794021bad280
Solveig Ranøien Engen <solvre@stud.ntnu.no>
2024-05-26 22:17:59 +0200

    trust the process

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 @app.route('/makePost')
 def make_post():
     return render_template('makePost.html')
