46fe7f4ad27263b9f4a5c2bf592ed2ae8efa00fd
Solveig Ranøien Engen <solvre@stud.ntnu.no>
2024-05-26 21:33:40 +0200

    fixed paths

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/makePost')
+def make_post():
+    return render_template('makePost.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
